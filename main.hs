344b09f5e7c41e84e92b22222fa52f326659bde7
jesseshieh <jesse.shieh.pub@gmail.com>
2014-05-19 16:26:26 -0700

    introduce dispatcher instead of executeCommand

--- main.hs
+++ main.hs
@@ -6,30 +6,31 @@ data Context = None | Home | Work | Shopping deriving (Show, Read, Eq)
 data Task = Task { text :: String, context :: Context } deriving (Show, Read)
 data Tasks = Tasks { tasks :: [Task] } deriving (Show, Read)
 
+dispatch :: [(String, [String] -> Tasks -> Tasks)]
+dispatch =  [ ("add", addAction)
+            , ("show", showAction)
+            , ("complete", completeAction)
+            , ("move", moveAction)
+            ]
+
 main = do
-	args <- getArgs
-	let (command, opt) = parseArgs args
+	(command:args) <- getArgs
+	let (Just action) = lookup command dispatch
 
 	-- TODO: what if the file is empty or does not exist?
 	let filename = "tasks.txt"
 	tasksString <- readFile filename
 	let tasks = read tasksString 
-	let newTasks = executeCommand command opt tasks
+	let newTasks = action args tasks
 
 	putStr $ showTasks newTasks
 
 	-- This line forces the entire file to be parsed so that we aren't
 	-- reading and writing to the same file simultaneously.
 	-- http://stackoverflow.com/questions/2527271/in-haskell-i-want-to-read-a-file-and-then-write-to-it-do-i-need-strictness-ann
-	if command /= Show then
+	if command /= "show" then
 		length tasksString `seq` (writeFile filename $ show newTasks)
 			else return ()
-
-parseArgs :: [String] -> (Command, String)
-parseArgs ["add", text] = (Add, text)
-parseArgs ["move", context] = (Move, context)
-parseArgs ["show"] = (Show, "")
-parseArgs ["complete"] = (Complete, "")
 
 showTasks :: Tasks -> String
 showTasks myTasks = do
@@ -37,20 +38,6 @@ showTasks myTasks = do
 
 showTask :: Task -> String
 showTask task = (text task) ++ ", " ++ (show $ context task)
-
-isInContext :: String -> Task -> Bool
-isInContext contextString task = (context task) == (readContext contextString)
-
-executeCommand :: Command -> String -> Tasks -> Tasks
--- executeCommand Show context myTasks = Tasks (filter (isInContext context) (tasks myTasks))
-executeCommand Show _ myTasks = myTasks
-executeCommand Add text myTasks = addTask text myTasks
-
--- TODO: can I just 'read' the context instead of providing my own translation function?
-executeCommand Move context myTasks = moveHeadTask (readContext context) myTasks
-
--- TODO: write completed tasks to an archive file.
-executeCommand Complete _ myTasks = completeHeadTask myTasks
 
 readContext :: String -> Context
 readContext context = stringToContext $ map toLower context
@@ -61,14 +48,15 @@ stringToContext "home" = Home
 stringToContext "work" = Work
 stringToContext "shopping" = Shopping
 
-addTask :: String -> Tasks -> Tasks
-addTask text myTasks = Tasks $ 
-	(tasks myTasks) ++ [Task text None]
+addAction :: [String] -> Tasks -> Tasks
+addAction [text] myTasks = Tasks $ (tasks myTasks) ++ [Task text None]
 
-completeHeadTask :: Tasks -> Tasks
-completeHeadTask myTasks = Tasks $ tail $ tasks myTasks
+showAction :: [String] -> Tasks -> Tasks
+showAction [] myTasks = myTasks
 
-moveHeadTask :: Context -> Tasks -> Tasks
-moveHeadTask newContext myTasks = Tasks $ (tail $ tasks myTasks) ++ [
-	let myTask = head $ tasks myTasks in
-		myTask { context = newContext }]
+completeAction :: [String] -> Tasks -> Tasks
+completeAction [] myTasks = Tasks $ tail (tasks myTasks)
+
+moveAction :: [String] -> Tasks -> Tasks
+moveAction [newContextString] myTasks = let newContext = (readContext newContextString) in
+	Tasks $ tail (tasks myTasks) ++ [(head (tasks myTasks)) { context = newContext }]
